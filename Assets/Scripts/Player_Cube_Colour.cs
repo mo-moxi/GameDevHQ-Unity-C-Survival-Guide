517992700b361a96665530db4cda53658858cdfb
mo-moxi <mem53211@gmail.com>
2021-02-09 19:36:11 +0000

    Challenge Review Master If-Statements P04

---
+++ Assets/Scripts/Player_Cube_Colour.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Cube_Colour : MonoBehaviour
+{
+    // Challenge
+    //Create a program that when you hit the space key you increment a score value. When that score is
+    //greater than 50, the cube turns green. At the start of the program, turn the cube red
+
+    public GameObject cube;
+    public int score;
+
+    void Start()
+    {
+        cube.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score += 10;
+        }
+        if (score >= 50)
+        {
+            cube.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+    }
+}
