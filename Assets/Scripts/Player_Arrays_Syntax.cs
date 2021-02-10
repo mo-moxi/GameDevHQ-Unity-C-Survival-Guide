707db5c261531ab6e55739596e7fefcdb210d2f0
mo-moxi <mem53211@gmail.com>
2021-02-10 14:10:17 +0000

    Array Syntax

---
+++ Assets/Scripts/Player_Arrays_Syntax.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Arrays_Syntax : MonoBehaviour
+{
+
+    public string[] names;
+    public string[] items = new string[5];
+    public int[] ages = new int[] { 4, 6, 9, 12, 15 };
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
