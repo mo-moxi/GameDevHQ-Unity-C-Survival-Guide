fb1a04ceaeb7992de08ff2ecba29f1571b2b4935
mo-moxi <mem53211@gmail.com>
2021-02-12 22:47:05 +0000

    Virtual Methods and Overridin

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Pet
+{
+protected override void Speak()
+{
+    Debug.Log("Bark! Bark!");
+}
+
+}
